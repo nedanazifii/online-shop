bc86ad58e91660a6989d9ce4520367fac1a8f85c
nedanazifii <nedanazifi8@gmail.com>
2025-07-02 12:58:27 +0300

    cart summery page

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def cart_summery(request):
+    return render(request, 'cart_summery.html')
+
+
+def cart_add(request):
+    pass
+
+
+def cart_delete(request):
+    pass
+
+
+def cart_update(request):
+    pass
