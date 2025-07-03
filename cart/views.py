6f2dff7c8690defadcccad20059829a3112c755e
nedanazifii <nedanazifi8@gmail.com>
2025-07-03 12:37:24 +0300

    product number

--- cart/views.py
+++ cart/views.py
@@ -7,15 +7,17 @@ from django.http import JsonResponse
 def cart_summery(request):
     cart = Cart(request)
     cart_products = cart.get_products()
-    return render(request, 'cart_summery.html', {'cart_products':cart_products})
+    quantities = cart.get_quantities()
+    return render(request, 'cart_summery.html', {'cart_products':cart_products, 'quantities':quantities})
 
 
 def cart_add(request):
     cart = Cart(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
         product = get_object_or_404(Product, id= product_id)
-        cart.add(product=product)
+        cart.add(product=product, quantity=product_qty)
 
         cart_quantity = cart.__len__()
 
