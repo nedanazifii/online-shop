37ebabef31d66b20b9a3e3d168fd1dc3b2a09ecc
nedanazifii <nedanazifi8@gmail.com>
2025-07-04 15:10:37 +0300

    Profile 1

--- store/models.py
+++ store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 import datetime
 
 class Category(models.Model):
@@ -17,6 +19,29 @@ class Customer(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(User, auto_now=True)
+    phone = models.CharField(max_length=25,blank=True)
+    address1 = models.CharField(max_length=400,blank=True)
+    address2 = models.CharField(max_length=400,blank=True)
+    city = models.CharField(max_length=30,blank=True)
+    state = models.CharField(max_length=30,blank=True)
+    zipcode = models.CharField(max_length=30,blank=True)
+    country = models.CharField(max_length=30,default="IRAN")
+
+    def __str__(self):
+        return self.user.username
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+post_save.connect(create_profile,sender=User)
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
